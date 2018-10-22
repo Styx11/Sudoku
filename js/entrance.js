9952d57efbe0eba7dd0faeb375eb02ac859f7281
styx11 <1654630248@qq.com>
2018-10-22 21:41:39 +0800

    :hammer: refactoring Grid

--- js/entrance.js
+++ js/entrance.js
@@ -1,11 +1,3 @@
 var grid = new Grid(9);
-grid.initGrid();
 
-while (true) {
-  if (grid.loop(grid.box[0]) && grid.loop(grid.box[2]) && grid.loop(grid.box[3]) && grid.loop(grid.box[5])) {
-    break;
-  } else {
-    grid.initGrid();
-  }
-}
 console.log(grid.cells);
--- js/grid.js
+++ js/grid.js
@@ -2,15 +2,20 @@ function Grid (size) {
   this.size = size ? size : 9;
   this.cells = [];
   this.box = [
-    {rowStart: 0, rowEnd: 2, colStart: 3, colEnd: 5},
-    {rowStart: 0, rowEnd: 2, colStart: 6, colEnd: 8},
-    {rowStart: 3, rowEnd: 5, colStart: 0, colEnd: 2},
-    {rowStart: 3, rowEnd: 5, colStart: 6, colEnd: 8},
-    {rowStart: 6, rowEnd: 8, colStart: 0, colEnd: 2},
-    {rowStart: 6, rowEnd: 8, colStart: 3, colEnd: 5}
+    {rowStart: 0, rowEnd: 2, colStart: 0, colEnd: 2},// box1
+    {rowStart: 0, rowEnd: 2, colStart: 3, colEnd: 5},// box2
+    {rowStart: 0, rowEnd: 2, colStart: 6, colEnd: 8},// box3
+    {rowStart: 3, rowEnd: 5, colStart: 0, colEnd: 2},// box4
+    {rowStart: 3, rowEnd: 5, colStart: 3, colEnd: 5},// box5
+    {rowStart: 3, rowEnd: 5, colStart: 6, colEnd: 8},// box6
+    {rowStart: 6, rowEnd: 8, colStart: 0, colEnd: 2},// box7
+    {rowStart: 6, rowEnd: 8, colStart: 3, colEnd: 5},// box8
+    {rowStart: 6, rowEnd: 8, colStart: 6, colEnd: 8},// box9
   ]
+  this.fillGrid();
 }
 
+// 获取区间内的随机数
 Grid.prototype.randomIndex = function (min, max) {
   if (!max) {
     max = min;
@@ -19,14 +24,19 @@ Grid.prototype.randomIndex = function (min, max) {
   return min + Math.floor(Math.random() * (max - min + 1));
 }
 
+// 获取一个随机数组
 Grid.prototype.shuffle = function (arr) {
   // 创建一个长度为size的数组
   // map方法要求一个可遍历的数组
-  var arr = arr ? arr : Array.apply(null, {length: this.size}).map(function (item, index) {
-    return index + 1;
-  })
+  var arr = arr
+    ? arr 
+    : Array.apply(null, {length: this.size})
+        .map(function (item, index) {
+          return index + 1;
+        })
   var length = this.size;
   var shuffled = new Array(length);
+  
   // 洗牌算法生成随机数组
   for (var index=0; index<length; index++) {
     var random = this.randomIndex(0, index);
@@ -40,39 +50,37 @@ Grid.prototype.shuffle = function (arr) {
 
 // 初始化栅格，包括初始化、建立对角线九宫格、建立反对角线九宫格
 Grid.prototype.initGrid = function () {
+  // 建立可遍历矩阵
   for (var row=0; row<this.size; row++) {
     this.cells[row] = Array.apply(null, {length: this.size});
   }
-
-  var maxCol = 3;
-  var col = 0;
-  var index = 0;
-  var shuffled = this.shuffle();
-
   // 建立对角线宫格
-  for (var row=0; row<maxCol; row++) {
-    if (row === 9) break;
-    for ( ; col<maxCol; col++) {
-      this.cells[row][col] = shuffled[index];
-      index++;
-    }
-    if (!((row + 1) % 3)) {
-      maxCol += 3;
-      index = 0;
-      shuffled = this.shuffle();
-    }
-    col = maxCol - 3;
+  for (var index=0; index<this.size; index+=4) {
+    this.fillBox(this.box[index])
   }
   // 填写反对角九宫格 (比填写中间九宫格效率高，所以一并执行)
   while (true) {
-    if (grid.loop(grid.box[1]) && grid.loop(grid.box[4])) {
+    if (this.fillBox(this.box[2]) && this.fillBox(this.box[6])) {
       break;
     }
   }
 }
 
+// 完成九宫格填写
+Grid.prototype.fillGrid = function () {
+  this.initGrid();
+  while (true) {
+    if (this.fillBox(this.box[1]) && this.fillBox(this.box[3]) && this.fillBox(this.box[5]) && this.fillBox(this.box[7])) {
+      break;
+    } else {
+      this.initGrid();
+    }
+  }
+  return true;
+}
+
 // 根据九宫格位置填写九宫格
-Grid.prototype.loop = function (box) {
+Grid.prototype.fillBox = function (box) {
   var rowEnd = box.rowEnd + 1,
     rowStart = box.rowStart;
   var colEnd = box.colEnd + 1,
@@ -84,7 +92,7 @@ Grid.prototype.loop = function (box) {
     var rowHash = this.hash(row, 'row');// 行哈希表
     for (col=colStart; col<colEnd; col++) {
       var colHash = this.hash(col, 'col');// 列哈希表
-      for (var index=0; index<9; index++) {
+      for (var index=0; index<shuffled.length; index++) {
         // 判断三个哈希表均无该值
         if (!rowHash[shuffled[index]] && !colHash[shuffled[index]] && !boxHash[shuffled[index]]) {
           boxHash[shuffled[index]] = true;// 更新九宫格哈希表
@@ -92,6 +100,7 @@ Grid.prototype.loop = function (box) {
           break;
         }
       }
+      // 若单元格填写失败，则此次九宫格创建失败
       if (!this.cells[row][col]) {
         return false;// 由于栈溢出，从递归改为循环
       }
@@ -106,17 +115,13 @@ Grid.prototype.hash = function (index, line) {
   if (line === 'col') {
     for (var row=0; row<this.size; row++) {
       if (!hash[this.cells[row][index]]) {
-        hash[this.cells[row][index]] = 'single';
-      } else {
-        hash[this.cells[row][index]] = 'surplus';
+        hash[this.cells[row][index]] = true;
       }
     }
   } else {
     for (var col=0; col<this.size; col++) {
       if (!hash[this.cells[index][col]]) {
-        hash[this.cells[index][col]] = 'single';
-      } else {
-        hash[this.cells[index][col]] = 'surplus';
+        hash[this.cells[index][col]] = true;
       }
     }
   }
