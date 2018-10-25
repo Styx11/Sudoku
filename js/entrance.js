50720ec71ee3400113455fa48f0dd6a73ef58da3
styx11 <1654630248@qq.com>
2018-10-25 21:21:42 +0800

    :heavy_plus_sign: add dependency of Vue.js

--- js/entrance.js
+++ js/entrance.js
@@ -1,8 +1,14 @@
 var grid = new Grid(9);
 // level = {
-//   easy: 3,
-//   normal: 4,
-//   hard: 6,
+//   easy: 1/3,
+//   normal: 1/2,
+//   hard: 1-1/3,
 // }
-var gameCells = grid.gameCells(6);
-console.log(gameCells);
+
+var sudoku = new Vue({
+  el: '#sudoku',
+  data: {
+    cells: grid.gameCells(4),
+  }
+})
+console.log(grid.cells);
