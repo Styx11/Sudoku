4828e5d16e95fd0504ade4f41c6c7b2d2ce40624
styx11 <1654630248@qq.com>
2018-11-01 20:09:26 +0800

    :sparkles: add keyboard toggle

--- components/keyboard.js
+++ components/keyboard.js
@@ -1,15 +1,36 @@
 Vue.component('key-input', {
+  data: function () {
+    return {
+      disabled: true,
+    }
+  },
+  computed: {
+    disabledBtn: function () {
+      return {
+        backgroundColor: this.disabled ? 'rgb(235, 236, 236)' : 'rgb(214, 215, 215)'
+      }
+    },
+    disabledRipple: function () {
+      return {
+        'mdui-ripple': !this.disabled
+      }
+    }
+  },
   template: "\
     <div class='keyboard'>\
       <div class='keyboard-input'>\
         <span \
-          v-for='n in 10'\
+          v-for='n in 9'\
           @click='inputNum(n)'\
-          class='keyboard-tile mdui-ripple'>{{ n }}</span>\
+          class='keyboard-tile'\
+          :style='disabledBtn'\
+          :class='disabledRipple'\
+          >{{ n }}</span>\
       </div>\
       <div class='keyboard-operate'>\
-        <span class='keyboard-tile mdui-ripple' @click='markTile'>标记</span>\
-        <span class='keyboard-tile mdui-ripple' @click='delNum'>清除</span>\
+        <span class='keyboard-tile' @click='markTile' :style='disabledBtn' :class='disabledRipple'>标记</span>\
+        <span class='keyboard-tile' @click='delNum' :style='disabledBtn' :class='disabledRipple'>清除</span>\
+        <span class='keyboard-tile mdui-ripple' @click='checkGrid'>查重</span>\
       </div>\
     </div>\
   ",
@@ -22,6 +43,16 @@ Vue.component('key-input', {
     },
     delNum: function () {
       bus.$emit('delNum');
+    },
+    checkGrid: function () {
+      bus.$emit('checkGrid');
     }
+  },
+  mounted: function () {
+    var _this = this;
+
+    bus.$on('keyboardToggle', function (disabled) {
+      _this.disabled = disabled;
+    })
   }
 })
