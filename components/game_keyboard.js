f7e36322c376fe1ea0ffd5f254150fba072f2413
styx11 <1654630248@qq.com>
2018-11-20 21:38:25 +0800

    :lipstick: add dark mode of game keyboard

--- components/game_keyboard.js
+++ components/game_keyboard.js
@@ -19,20 +19,16 @@ Vue.component('game-keyboard', {
           v-for='n in 9'\
           @click='inputNum(n)'\
           class='keyboard-tile mdui-shadow-1'\
-          :style='disabledClass(btnDisabled || n>keyRange).btn'\
-          :class='disabledClass(btnDisabled || n>keyRange).ripple'\
+          :class='disabledClass(btnDisabled || n>keyRange)'\
           >{{ n }}</span>\
       </div>\
       <div class='keyboard-operate'>\
         <span class='keyboard-tile mdui-shadow-1' @click='markTile'\
-        :style='disabledClass(btnDisabled).btn'\
-        :class='disabledClass(btnDisabled).ripple'>标记</span>\
+        :class='disabledClass(btnDisabled)'>标记</span>\
         <span class='keyboard-tile mdui-shadow-1' @click='delNum'\
-        :style='disabledClass(btnDisabled).btn'\
-        :class='disabledClass(btnDisabled).ripple'>清除</span>\
+        :class='disabledClass(btnDisabled)'>清除</span>\
         <span v-if='!gameComplete' class='keyboard-tile mdui-ripple mdui-shadow-1' @click='checkGrid'\
-        :style='disabledClass(checkDisabled).btn'\
-        :class='disabledClass(checkDisabled).ripple'>查错</span>\
+        :class='disabledClass(checkDisabled)'>查错</span>\
       </div>\
     </div>\
   ",
@@ -53,16 +49,10 @@ Vue.component('game-keyboard', {
 
     // 由是否可用，返回相应的样式与类
     disabledClass: function (disabled) {
-      var button = {
-        backgroundColor: disabled ? 'rgb(235, 236, 236)' : 'rgb(214, 215, 215)'
-      }
-      var ripple = {
-        'mdui-ripple': !disabled
-      }
-
       return {
-        btn: button,
-        ripple: ripple
+        'mdui-ripple': !disabled,
+        'keyboard-tile-abled': !disabled,
+        'keyboard-tile-disabled': disabled
       }
     }
   },
