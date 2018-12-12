e1b17acdf9f4957abc2f655a9f1e9427903deda0
styx11 <1654630248@qq.com>
2018-12-12 20:55:56 +0800

    :bug: fix header's shaking in help pages switching

--- pages/help/Help.js
+++ pages/help/Help.js
@@ -1,6 +1,26 @@
 var Help = {
+  data: function () {
+    return {
+      helpRoute: ''
+    }
+  },
   template: '\
-  <div class="help">\
+  <div class="help" :class="helpRouteClass">\
       <router-view></router-view>\
-  </div>'
+  </div>',
+  created: function () {
+    this.helpRoute = this.$router.history.current.path;// 获取当前路由路径
+  },
+  watch: {
+    '$route': function () {// 监听路由
+      this.helpRoute = this.$router.history.current.path;
+    }
+  },
+  computed: {
+    helpRouteClass: function () {
+      return {
+        'helpRoute': this.helpRoute === '/help'// 当切换至help-home页时，增加overflow，解决header抖动
+      }
+    }
+  }
 }
