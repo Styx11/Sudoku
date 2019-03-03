e0698a490b9051bbc4f4867c6dfcb57fae0b1a2b
styx11 <1654630248@qq.com>
2019-03-03 17:11:38 +0800

    :recycle: refactoring header with module standard

--- src/common/mdui_header.js
+++ src/common/mdui_header.js
@@ -1,4 +1,10 @@
-Vue.component('mdui-header', {
+import GameTimer from './game_timer';
+
+export default {
+  name: 'MduiHeader',
+  components: {
+    GameTimer
+  },
   props: {
     github: {
       type: String,
@@ -6,7 +12,7 @@ Vue.component('mdui-header', {
     },
     title: {
       type: String,
-      require: true
+      require: false
     },
     timer: {
       type: Boolean,
@@ -16,39 +22,61 @@ Vue.component('mdui-header', {
   data: function () {
     return {
       eggCount: 0
-    }
+    };
   },
-  template: '\
-  <div class="mdui-appbar mdui-appbar-fixed">\
-    <div class="mdui-toolbar mdui-color-theme">\
-      <slot name="drawer"></slot>\
-      <span @click="eggCount++">\
-        <span v-if="timer" class="mdui-text-color-white">\
-          <span class="timer-title mdui-typo-title">{{ title }}</span>\
-          <game-timer></game-timer>\
-        </span>\
-        <span v-else class="mdui-text-color-white mdui-typo-title">{{ title }}</span>\
-      </span>\
-      <div class="mdui-toolbar-spacer"></div>\
-      <a v-if="github" :href="github" target="_blank" class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white" mdui-tooltip="{content: \'查看 Github\'}">\
-        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 36 36" enable-background="new 0 0 36 36" xml:space="preserve" class="mdui-icon" style="width: 24px;height:24px;">\
-          <path fill-rule="evenodd" clip-rule="evenodd" fill="#ffffff" d="M18,1.4C9,1.4,1.7,8.7,1.7,17.7c0,7.2,4.7,13.3,11.1,15.5\
-          c0.8,0.1,1.1-0.4,1.1-0.8c0-0.4,0-1.4,0-2.8c-4.5,1-5.5-2.2-5.5-2.2c-0.7-1.9-1.8-2.4-1.8-2.4c-1.5-1,0.1-1,0.1-1\
-          c1.6,0.1,2.5,1.7,2.5,1.7c1.5,2.5,3.8,1.8,4.7,1.4c0.1-1.1,0.6-1.8,1-2.2c-3.6-0.4-7.4-1.8-7.4-8.1c0-1.8,0.6-3.2,1.7-4.4\
-          c-0.2-0.4-0.7-2.1,0.2-4.3c0,0,1.4-0.4,4.5,1.7c1.3-0.4,2.7-0.5,4.1-0.5c1.4,0,2.8,0.2,4.1,0.5c3.1-2.1,4.5-1.7,4.5-1.7\
-          c0.9,2.2,0.3,3.9,0.2,4.3c1,1.1,1.7,2.6,1.7,4.4c0,6.3-3.8,7.6-7.4,8c0.6,0.5,1.1,1.5,1.1,3c0,2.2,0,3.9,0,4.5\
-          c0,0.4,0.3,0.9,1.1,0.8c6.5-2.2,11.1-8.3,11.1-15.5C34.3,8.7,27,1.4,18,1.4z"></path>\
-        </svg>\
-      </a>\
-      <slot name="menu"></slot>\
-    </div>\
-  </div>\
-  ',
   watch: {
-    eggCount: function () {
+    eggCount() {
       if (this.eggCount < 10) return;
       this.eggCount = 0;
       this.$emit('openEgg');
     }
+  },
+  render() {
+    // 计时器
+    const timer = () => {
+      if (this.timer) {
+        return (
+          <GameTimer/>
+        );
+      }
+    };
+
+    // github 图标
+    const githubIcon = () => {
+      if (!this.github) return;
+      return (
+        <a href={this.github} target='_blank' class='mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white' mdui-tooltip='{content: "查看 Github"}'>
+          <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 36 36" enable-background="new 0 0 36 36" xmlSpace="preserve" class="mdui-icon" style={{width: '24px', height:' 24px'}}>
+            <path fill-rule="evenodd" clip-rule="evenodd" fill="#ffffff" d="M18,1.4C9,1.4,1.7,8.7,1.7,17.7c0,7.2,4.7,13.3,11.1,15.5
+            c0.8,0.1,1.1-0.4,1.1-0.8c0-0.4,0-1.4,0-2.8c-4.5,1-5.5-2.2-5.5-2.2c-0.7-1.9-1.8-2.4-1.8-2.4c-1.5-1,0.1-1,0.1-1
+            c1.6,0.1,2.5,1.7,2.5,1.7c1.5,2.5,3.8,1.8,4.7,1.4c0.1-1.1,0.6-1.8,1-2.2c-3.6-0.4-7.4-1.8-7.4-8.1c0-1.8,0.6-3.2,1.7-4.4
+            c-0.2-0.4-0.7-2.1,0.2-4.3c0,0,1.4-0.4,4.5,1.7c1.3-0.4,2.7-0.5,4.1-0.5c1.4,0,2.8,0.2,4.1,0.5c3.1-2.1,4.5-1.7,4.5-1.7
+            c0.9,2.2,0.3,3.9,0.2,4.3c1,1.1,1.7,2.6,1.7,4.4c0,6.3-3.8,7.6-7.4,8c0.6,0.5,1.1,1.5,1.1,3c0,2.2,0,3.9,0,4.5
+            c0,0.4,0.3,0.9,1.1,0.8c6.5-2.2,11.1-8.3,11.1-15.5C34.3,8.7,27,1.4,18,1.4z"></path>
+         </svg>
+        </a>
+      );
+    };
+
+    // title 类
+    const titleClass = {
+      'mdui-typo-title': true,
+      'timer-title': this.timer
+    };
+
+    return (
+      <div class='mdui-appbar mdui-appbar-fixed'>
+        <div class='mdui-toolbar mdui-color-theme'>
+          {this.$slots.drawer}
+          <span onClick={() => this.eggCount++} class='mdui-text-color-white'>
+              <span class={titleClass}>{this.title}</span>
+              {timer()}
+          </span>
+          <div class='mdui-toolbar-spacer'></div>
+          {githubIcon()}
+          {this.$slots.menu}
+        </div>
+      </div>
+    );
   }
-})
+};
