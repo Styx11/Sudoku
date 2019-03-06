f506e642c801451568ebdac2601a8b6796dd9302
styx11 <1654630248@qq.com>
2019-03-06 21:31:50 +0800

    :recycle: refactor game keyboard with ES6 standard

--- src/pages/home/components/game_keyboard.js
+++ src/pages/home/components/game_keyboard.js
@@ -1,4 +1,5 @@
-Vue.component('game-keyboard', {
+export default {
+  name: 'GameKeyboard',
   props: {
     keyRange: {
       type: Number,
@@ -9,11 +10,11 @@ Vue.component('game-keyboard', {
       require: false
     }
   },
-  data: function () {
+  data () {
     return {
       numDisabled: true,
       checkDisabled: true,
-      opreateDisabled: true,
+      operateDisabled: true,
       numberSolved: {
         1: false,
         2: false,
@@ -25,87 +26,103 @@ Vue.component('game-keyboard', {
         8: false,
         9: false 
       }
-    }
+    };
   },
-  template: "\
-    <div class='keyboard'>\
-      <div class='keyboard-input'>\
-        <span \
-          v-for='n in 9'\
-          @click='inputNum(n)'\
-          class='keyboard-tile mdui-shadow-1'\
-          :class='numClass(n)'\
-          >{{ n }}</span>\
-      </div>\
-      <div class='keyboard-operate'>\
-        <span class='keyboard-tile mdui-shadow-1' @click='markTile'\
-        :class='operateClass(opreateDisabled)'>标记</span>\
-        <span class='keyboard-tile mdui-shadow-1' @click='delNum'\
-        :class='operateClass(opreateDisabled, true)'>清除</span>\
-        <span class='keyboard-tile mdui-shadow-1' @click='checkGrid'\
-        :class='operateClass(checkDisabled)'>查错</span>\
-      </div>\
-    </div>\
-  ",
-  methods: {
-    inputNum: function (n) {
-      if (n <= this.keyRange && !this.numberSolved[n]) return bus.$emit('inputNum', n);
-    },
-    markTile: function () {
-      bus.$emit('markTile');
-    },
-    delNum: function () {
-      bus.$emit('delNum');
-    },
-    checkGrid: function () {
-      if (!this.checkDisabled) return bus.$emit('checkGrid');
-    },
+  mounted () {
+    // 监听操作键是否可用
+    this.bus.$on('operateDisabled', disabled => this.operateDisabled = disabled);
 
+    // 监听查错按钮是否可用
+    this.bus.$on('checkDisabled', disabled => this.checkDisabled = disabled);
+
+    // 监听数字键盘是否可用
+    this.bus.$on('numDisabled', disabled => this.numDisabled = disabled);
+
+    // 监听数字完成量
+    this.bus.$on('numberSolved', this.handleNums);
+  },
+  methods: {
+    inputNum (n) {
+      if (n <= this.keyRange && !this.numberSolved[n] && !this.numDisabled) {
+        this.bus.$emit('inputNum', n);
+      }
+    },
+    markTile () {
+      if (!this.operateDisabled) {
+        this.bus.$emit('markTile');
+      }
+    },
+    delNum () {
+      if (!this.operateDisabled) {
+        this.bus.$emit('delNum');
+      }
+    },
+    checkGrid () {
+      if (!this.checkDisabled) {
+        this.bus.$emit('checkGrid');
+      }
+    },
     // 由是否可用，返回相应的样式与类
-    operateClass: function (disabled, delBtn) {
+    operateClass (disabled, delBtn) {
       return {
         'mdui-ripple': delBtn ? false : !disabled,// 取消清除按钮的涟漪效果，修复禁用后涟漪动画错位bug
         'keyboard-tile-abled': !disabled,
         'keyboard-tile-disabled': disabled
-      }
+      };
     },
-    numClass: function (num) {
-      var keyRange = this.keyRange;// 数字量若达到上限，则禁用
-      var disabled = this.numDisabled || num > keyRange || this.numberSolved[num];
-
+    numClass (num) {
+      const keyRange = this.keyRange;// 数字量若达到上限，则禁用
+      const disabled = this.numDisabled || num > keyRange || this.numberSolved[num];
       return {
         'mdui-ripple': !disabled || this.disableSolved,// 开启数字量禁用时，开启涟漪
         'keyboard-tile-abled': !disabled,
         'keyboard-tile-disabled': disabled
-      }
+      };
     },
-
     // 处理数字完成量
-    handleNums: function (nums) {
-      for (var n in nums) {
+    handleNums (nums) {
+      for (let n in nums) {
         this.numberSolved[n] = this.disableSolved && nums ? (nums[n] >= this.keyRange) : false;
       }
     }
   },
-  mounted: function () {
-    var _this = this;
+  render () {
+    const numKeys = () => {
+      return Array.apply(null, {length: 9})
+        .map((n, i) => {
+          return (
+            <span
+              onClick={this.inputNum.bind(this, i + 1)}
+              class='keyboard-tile mdui-shadow-1'
+              {...{class: this.numClass(i + 1)}}
+            >{ i + 1 }</span>
+          );
+        });
+    };
 
-    // 监听数字键盘是否可用
-    bus.$on('numDisabled', function (disabled) {
-      _this.numDisabled = disabled;
-    })
-
-    // 监听操作键是否可用
-    bus.$on('opreateDisabled', function (disabled) {
-      _this.opreateDisabled = disabled;
-    })
-
-    // 监听差错按钮是否可用
-    bus.$on('checkDisabled', function (disabled) {
-      _this.checkDisabled = disabled;
-    })
-
-    // 监听数字完成量
-    bus.$on('numberSolved', this.handleNums)
+    return (
+      <div class='keyboard'>
+        <div class='keyboard-input'>
+          {numKeys()}
+        </div>
+        <div class='keyboard-operate'>
+          <span
+          class='keyboard-tile mdui-shadow-1'
+          {...{class: this.operateClass(this.operateDisabled)}}
+          onClick={this.markTile.bind(this)}
+          >标记</span>
+          <span
+          class='keyboard-tile mdui-shadow-1'
+          {...{class: this.operateClass(this.operateDisabled, true)}}
+          onClick={this.delNum.bind(this)}
+          >清除</span>
+          <span
+          class='keyboard-tile mdui-shadow-1'
+          {...{class: this.operateClass(this.checkDisabled)}}
+          onClick={this.checkGrid.bind(this)}
+          >查错</span>
+        </div>
+      </div>
+    );
   }
-})
+};
