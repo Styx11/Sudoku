a29af87bf5ae8399c5ee192ad4e66a1b3b65cb1d
styx11 <1654630248@qq.com>
2019-03-13 20:35:24 +0800

    :rocket: update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import HomePage from '@/pages/home/Home';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    
+    { path: '', component: HomePage }
   ]
 });
