5cd5535f7bc036262c04a31057714cb05d7a9354
styx11 <1654630248@qq.com>
2019-03-14 20:08:44 +0800

    :recycle: refactor HELP pages with ES6

--- src/pages/help/Help.js
+++ src/pages/help/Help.js
@@ -1,29 +1,35 @@
-var Help = {
-  props: ['slideName'],
-  data: function () {
+export default {
+  name: 'HelpPage',
+  props: {
+    slideName: String,
+  },
+  data () {
     return {
       helpRoute: ''
+    };
+  },
+  computed: {
+    helpRouteClass () {
+      return {
+        'helpRoute': this.helpRoute === '/help'// 当切换至help-home页时，增加overflow，解决header抖动
+      };
     }
   },
-  template: '\
-  <div class="help" :class="helpRouteClass">\
-    <transition :enter-active-class="slideName">\
-      <router-view></router-view>\
-    </transition>\
-  </div>',
-  created: function () {
-    this.helpRoute = this.$router.history.current.path;// 获取当前路由路径
+  created () {
+    this.helpRoute = this.$router.history.current.path;// 获取当前路由路径    
   },
   watch: {
-    '$route': function () {// 监听路由
+    '$route' () {// 监听路由
       this.helpRoute = this.$router.history.current.path;
     }
   },
-  computed: {
-    helpRouteClass: function () {
-      return {
-        'helpRoute': this.helpRoute === '/help'// 当切换至help-home页时，增加overflow，解决header抖动
-      }
-    }
+  render () {
+    return (
+      <div class='help' {...{class: this.helpRouteClass}}>
+        <transition enter-active-class={this.slideName}>
+          <router-view/>
+        </transition>
+      </div>
+    );
   }
-}
+};
--- src/pages/help/components/help_home.js
+++ src/pages/help/components/help_home.js
@@ -1,33 +1,41 @@
-var HelpPage = {
-  template: '\
-    <div class="help-home">\
-      <mdui-header title="帮助">\
-        <template slot="drawer">\
-          <a class="mdui-btn mdui-btn-icon mdui-ripple mdui-text-color-white" @click="goBack">\
-            <i class="mdui-icon material-icons">&#xe5c4;</i>\
-          </a>\
-        </template>\
-      </mdui-header>\
-      <ul class="mdui-list">\
-        <li class="mdui-subheader mdui-text-color-theme-accent">规则</li>\
-        <router-link to="/help/how">\
-          <li class="mdui-list-item mdui-ripple">怎么玩</li>\
-        </router-link>\
-        <router-link to="/help/trick">\
-          <li class="mdui-list-item mdui-ripple">基本技巧</li>\
-        </router-link>\
-        <li class="mdui-subheader mdui-text-color-theme-accent">反馈</li>\
-        <li class="mdui-list-item mdui-ripple">\
-          <a href="https://github.com/Styx11/Sudoku/issues">BUG报告</a>\
-        </li>\
-      </ul>\
-    </div>\
-  ',
+import MduiHeader from '@/common/mdui_header';
+
+export default {
+  name: 'HelpHome',
+  components: {
+    MduiHeader,
+  },
   methods: {
     goBack () {
       window.history.length > 1
         ? this.$router.go(-1)
-        : this.$router.push('/')
+        : this.$router.push('/');
     }
+  },
+  render () {
+    return (
+      <div class='help-home'>
+      <MduiHeader title='帮助'>
+        <template slot='drawer'>
+          <a class='mdui-btn mdui-btn-icon mdui-ripple mdui-text-color-white' onClick={this.goBack.bind(this)}>
+            <i class='mdui-icon material-icons'>&#xe5c4;</i>
+          </a>
+        </template>
+      </MduiHeader>
+      <ul class='mdui-list'>
+        <li class='mdui-subheader mdui-text-color-theme-accent'>规则</li>
+        <router-link to='/help/how'>
+          <li class='mdui-list-item mdui-ripple'>怎么玩</li>
+        </router-link>
+        <router-link to='/help/trick'>
+          <li class='mdui-list-item mdui-ripple'>基本技巧</li>
+        </router-link>
+        <li class='mdui-subheader mdui-text-color-theme-accent'>反馈</li>
+        <li class='mdui-list-item mdui-ripple'>
+          <a href='https://github.com/Styx11/Sudoku/issues'>BUG报告</a>
+        </li>
+      </ul>
+    </div>
+    );
   }
-}
+};
