f7b648847715c74a7dc2ba417d47acbf04842049
styx11 <1654630248@qq.com>
2019-03-26 21:10:54 +0800

    :bento: add store of game
    :bento: add store of game

---
+++ src/store/gameStore/index.js
@@ -0,0 +1,19 @@
+import LSManager from '@/script/localStorage_manager';
+import Grid from '@/script/grid';
+
+export default {
+  state () {
+    return {
+      
+    };
+  },
+  getters: {
+    
+  },
+  mutations: {
+    
+  },
+  actions: {
+    
+  }
+};
